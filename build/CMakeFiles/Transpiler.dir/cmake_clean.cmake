c08905d2b5ba1514f39a644fb23e67749aa37f27
Github11200 <pateljinay@outlook.com>
2025-10-21 23:37:34 -0700

    working on project setup

---
+++ build/CMakeFiles/Transpiler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Transpiler.dir/main.cpp.o"
+  "CMakeFiles/Transpiler.dir/main.cpp.o.d"
+  "Transpiler"
+  "Transpiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Transpiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
